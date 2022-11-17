48447ef6ea9c160a9b9a02cf11c6e042a3e05c35
mallvinorz <mallvino1216@gmail.com>
2022-11-17 11:26:14 +0700

    add script growing plant

--- Assets/Plants.cs
+++ Assets/Plants.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class Plants : MonoBehaviour
 {
+    public float timer = 0f;
+    public float growTime = 12f;
+    public float maxSize = 2f;
+
+    public bool isMaxSize = false;
+
     // Start is called before the first frame update
      private void Start(){
+        if(isMaxSize == false){
+            StartCoroutine(Grow());
+        }
         GameObject player = GameObject.FindGameObjectWithTag("Player");     
         Debug.Log(player);
         Physics.IgnoreCollision(player.GetComponent<CapsuleCollider>(), GetComponent<BoxCollider>());
+    }
+
+    private IEnumerator Grow(){
+        Vector3 startScale = transform.localScale;
+        Vector3 maxScale = new Vector3(maxSize, maxSize,maxSize);
+
+        do{
+            transform.localScale = Vector3.Lerp(startScale, maxScale, timer/growTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        while(timer < growTime);
+
+        isMaxSize = true;
     }
 }
