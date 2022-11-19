88b02610f2000c57448ebe82b4e5fe732a4c06b3
SeptAlfauzan <alfauzansepta@gmail.com>
2022-11-19 11:52:56 +0700

    add pond and water shader

---
+++ Assets/ScriptableObjects/Tools Data/ToolsItemData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tool Item Data")]
+
+public class ToolsItemData : ScriptableObject
+{
+    public GameObject prefabEffect;
+    public int staminaCost;
+}
